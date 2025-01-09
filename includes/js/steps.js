0da5c607dc4172b67a216d3f4bf9f03463a9394e
nonwill <nonwill@hotmail.com>
2025-01-09 13:36:17 +0800

    +

--- includes/js/steps.js
+++ includes/js/steps.js
@@ -1,9 +1,8 @@
 $(function(){
 	$('#content').css('font-size','100%');
   stepCount = 0;
-
-  console.log('%ci12bretro', 'font-weight: 900; font-size: 40px; color: #f00; text-shadow: 3px 3px 0 #000, 4px 4px 0 #fff; border-bottom: 8px double #f00; padding: 0 8px;');
-  console.log('%chttps://youtube.com/c/i12bretro', 'font-size: 9px; margin-left: 60px;');
+  /*console.log('%ci12bretro', 'font-weight: 900; font-size: 40px; color: #f00; text-shadow: 3px 3px 0 #000, 4px 4px 0 #fff; border-bottom: 8px double #f00; padding: 0 8px;');
+  console.log('%chttps://youtube.com/c/i12bretro', 'font-size: 9px; margin-left: 60px;');*/
   stepsText = [];
   $('textarea').each(function(i,e){
     theTextarea = $(this);
@@ -69,16 +68,15 @@ $(function(){
     });
   });
 
-  if(window.location.hostname.toLowerCase() !== 'i12bretro.lan'){
-    $('#gridContainer').append('<div>&nbsp;</div><div id="footerLinks"></div>');
-    $.ajax({ url: './includes/sites.json', type: 'get', dataType: 'json', success: function(r){
-        $.each(r, function(k,v){
-          $('#footerLinks').append(`<a href="${r[k].url}" target="_blank"><img src="${r[k].icon}" alt="${r[k].text}" title="${r[k].text}" /></a>`);
-        });
-      }
-    });
-    $('body').prepend('<div style="position: fixed; top: 15px; right: 15px; z-index: 100;"><button id="fontSizeUp">&#9650;<span>A</span></button>&nbsp;&nbsp;<button id="fontSizeDown"><span>A</span>&#9660;</button></div>');
-  }
+  $('#gridContainer').append('<div>&nbsp;</div><div id="footerLinks"></div>');
+  $.ajax({ url: './includes/sites.json', type: 'get', dataType: 'json', success: function(r){
+      $.each(r, function(k,v){
+        /*$('#footerLinks').append(`<a href="${r[k].url}" target="_blank"><img src="${r[k].icon}" alt="${r[k].text}" title="${r[k].text}" /></a>`);*/
+      });
+    }
+  });
+   
+  $('body').prepend('<div style="position: fixed; top: 15px; right: 15px; z-index: 100;"><button id="fontSizeUp">&#9650;<span>A</span></button>&nbsp;&nbsp;<button id="fontSizeDown"><span>A</span>&#9660;</button></div>');
 	
 	fontAdjustment = 0;
 	fontSizeDir = '';
